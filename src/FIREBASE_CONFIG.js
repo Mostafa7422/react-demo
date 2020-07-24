afea368687360402e49bab372a4aadb1d4b16ee1
Mostafa7422 <46670566+Mostafa7422@users.noreply.github.com>
2020-07-24 15:55:38 +0200

    Add files via upload

---
+++ src/FIREBASE_CONFIG.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDfmiFzW6FLqiiExg3l__wY6KeYPn89Xc0",
+    authDomain: "faq-bot-oytlhp.firebaseapp.com",
+    databaseURL: "https://faq-bot-oytlhp.firebaseio.com",
+    projectId: "faq-bot-oytlhp",
+    storageBucket: "faq-bot-oytlhp.appspot.com",
+    messagingSenderId: "224023120285",
+    appId: "1:224023120285:web:2ed53b569d8305a66e4774"
+  };
